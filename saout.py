329f7c89cb5492ec9a0ffdb713b0c709a1cb051b
Andrey Bykov <andbykov@keepdata.ru>
2020-05-12 22:56:31 +0300

    adding saout

---
+++ saout.py
@@ -0,0 +1,82 @@
+def key_checker(key,dict):
+   orgkey = key
+   i = 2
+   while key in dict:
+      key = orgkey
+      key += '_%s'%i
+      i += 1
+   return key
+
+def out_serv_recom_parser(file):
+   file.seek(0)
+   block = []
+   for ln in file:
+      if ln.startswith('sainfo lsservicerecommendation'):
+         data = next(file)
+         header = data.rstrip()
+         recommendations = ''
+         values = {}
+         while data != '\n':
+            data = next(file)
+            recommendations+=data.rstrip()
+         values.update({header:recommendations})
+         block.append(values)
+         return block
+
+def out_blk_space_parser(file,cmd):
+   file.seek(0)
+   block = []
+   for ln in file:
+      if ln.startswith(cmd):
+         data = next(file)
+         header = data.rstrip().split()
+         data = next(file)
+         while data != '\n':
+            split_data = data.rstrip().split()
+            values = {}
+            for i in range(0, len(header)):
+               if i>len(split_data)-1:
+                  values.update({header[i]: ''})
+               else:
+                  values.update({header[i]: split_data[i]})
+            block.append(values)
+            data = next(file)
+         return block
+
+def out_line_parser(file,cmd):
+   file.seek(0)
+   block = []
+   for ln in file:
+      if ln.startswith(cmd):
+         data = next(file)
+         values = {}
+         while data!='\n':
+            split_data = data.rstrip().split()
+            name=split_data[0]
+            if len(split_data)>1:
+               value=''
+               for i in range(1,len(split_data)):
+                  value += split_data[i]
+                  if (len(split_data) > 1 and i < len(split_data)-1):
+                     value += ' '
+            else:
+               value='else'
+            if name in values:
+               name = key_checker(name,values)
+               values.update({name: value})
+            else:
+               values.update({name:value})
+            data=next(file)
+         block.append(values)
+   return block
+
+def sa_out(infile):
+   file = open(infile,'+r')
+   sa_data={}
+   sa_data.update({'lsservicestatus': out_line_parser(file, 'sainfo lsservicestatus')})
+   sa_data.update({'lscmdstatus': out_line_parser(file, 'sainfo lscmdstatus')})
+   sa_data.update({'lsservicenodes': out_blk_space_parser(file, 'sainfo lsservicenodes')})
+   sa_data.update({'lsservicerecommendation': out_serv_recom_parser(file)})
+   sa_data.update({'lshardware': out_line_parser(file, 'sainfo lshardware')})
+   file.close()
+   return sa_data
--- snap_get_node_level.py
+++ snap_get_node_level.py
@@ -2,6 +2,7 @@
 import os
 from svcoutglob import svc_out_glob
 from svcoutint import svc_out_int
+from saout import sa_out
 
 os.chdir('logs')
 dirs = filter(os.path.isdir, os.listdir())
@@ -9,11 +10,16 @@ print('------')
 for dir in dirs:
     files = os.listdir("%s/dumps"%dir)
     for file in files:
+       if file.startswith('saout.'):
+           saout_data = sa_out("%s/dumps/%s"%(dir,file))
+           print(saout_data['lsservicerecommendation'][0]['service_action'])
+           print(saout_data['lshardware'])
        if file.startswith('svcout.int'):
           svc_info_int = svc_out_int("%s/dumps/%s"%(dir,file))
-          print(svc_info_int['lsenclosure_delim'][0])
-          for lsarraymember in svc_info_int['lsarraymember']:
-            print(lsarraymember)
+          #print (svc_info_int['lsdrive'])
+          #print(svc_info_int['lsenclosure_delim'][0])
+          #for lsdrive in svc_info_int['lsdrive']:
+          #  print("id: %s - slotid: %s"%(lsdrive['id'],lsdrive['slot_id']))
        if file.startswith('svcout.7'):
           svc_info_glob = svc_out_glob("%s/dumps/%s"%(dir,file))
           print(svc_info_glob['lsnode'][0]['enclosure_serial_number'])
