90a6d08422f17479cabead9b141c378cec3a5923
ByAndrey <andbykov@hotmail.com>
2020-09-29 23:13:58 +0300

    adding audit

---
+++ audit.py
@@ -0,0 +1,22 @@
+def out_line_parser(file,cmd):
+   file.seek(0)
+   block = []
+   for ln in file:
+      if ln.startswith(cmd):
+         data = next(file)
+         values = {}
+         while data!='\n':
+            split_data = data.rstrip().split(':')
+            name=split_data[0]
+            value=split_data[1]
+            values.update({name:value})
+            data=next(file)
+         block.append(values)
+   return block
+
+def audit(infile):
+    file = open(infile,'+r')
+    audit_log = {}
+    audit_log.update({'audit': out_line_parser(file, 'Auditlog Entry')}})
+    file.close()
+    return audit_log
